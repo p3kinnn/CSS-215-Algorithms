28bbbb9764795671aa0fca2d4a4a42739369e182
Yelaman Yelalyssov <elalisovelaman@gmail.com>
2026-09-25 12:44:13 +0500

    Time: 12 ms (48.05%), Space: 47.3 MB (59.34%) - LeetHub

---
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        List<Integer> list = new ArrayList<>();
+        if (s == null || s.length() == 0 || words == null || words.length == 0) {
+            return list;
+        }
+
+        int wordLen = words[0].length();
+        int numWords = words.length;
+
+        Map<String, Integer> wordCount = new HashMap<>();
+        for (String w : words) {
+            wordCount.put(w, wordCount.getOrDefault(w, 0) + 1);
+        }
+
+        for (int i = 0; i < wordLen; i++) {
+            int left = i;
+            int right = i;
+            int count = 0;
+            Map<String, Integer> currentCount = new HashMap<>();
+
+            while (right + wordLen <= s.length()) {
+                String word = s.substring(right, right + wordLen);
+                right += wordLen;
+
+                if (wordCount.containsKey(word)) {
+                    currentCount.put(word, currentCount.getOrDefault(word, 0) + 1);
+                    count++;
+
+                    while (currentCount.get(word) > wordCount.get(word)) {
+                        String leftWord = s.substring(left, left + wordLen);
+                        currentCount.put(leftWord, currentCount.get(leftWord) - 1);
+                        count--;
+                        left += wordLen;
+                    }
+
+                    if (count == numWords) {
+                        list.add(left);
+                    }
+                } else {
+                    currentCount.clear();
+                    count = 0;
+                    left = right;
+                }
+            }
+        }
+        return list;
+    }
+}
